665b3795aea5fddd5625349ef2383ab69ebf4dce
italo <italovinicius2018@gmail.com>
2022-07-31 04:16:03 -0300

    change the api route

--- js/getMovie.js
+++ js/getMovie.js
@@ -1,14 +1,14 @@
 export class getMovie {
   static search(movieTitle){
-    const endpoint = `http://www.omdbapi.com/?t=${movieTitle}`
+    const endpoint = `https://www.omdbapi.com/?t=${movieTitle}&apikey=29098c83&`
 
     return fetch(endpoint)
     .then(data => data.json())
-    .then({Title, Year, Runtime, Poster} => {
+    .then(({Title, Year, Runtime, Poster}) => ({
       Title,
       Year,
       Runtime,
       Poster
-    })
+    }))
   }
 }
